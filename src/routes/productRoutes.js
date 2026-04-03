5665457ff8fafac5287cb255db7f3cda016a51f2
Roman1019 <142348028+Roman1019@users.noreply.github.com>
2026-04-04 00:36:09 +0300

    commit 1

--- src/routes/productRoutes.js
+++ src/routes/productRoutes.js
@@ -1,12 +1,28 @@
+// import express from "express";
+// import {
+//   getProducts,
+//   createProduct,
+// } from "../controllers/productController.js";
+
+// const router = express.Router();
+
+// router.get("/", getProducts);
+// router.post("/", createProduct);
+
+// export default router;
 import express from "express";
-import {
-  getProducts,
-  createProduct,
-} from "../controllers/productController.js";
+import Product from "../models/product.js";
 
 const router = express.Router();
 
-router.get("/", getProducts);
-router.post("/", createProduct);
+// GET /products
+router.get("/", async (req, res) => {
+  try {
+    const products = await Product.find(); // витягує всі продукти з Mongo
+    res.json(products);
+  } catch (err) {
+    res.status(500).json({ message: "Щось пішло не так", error: err });
+  }
+});
 
 export default router;
